d8cafde811fa3b37d916d190960d1640b75819e5
epa <epa@localhost.localdomain>
2017-10-12 15:25:08 +0100

    More revision added.

--- revision/script1.sh
+++ revision/script1.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Welcome the user
+# Welcome the user.
+echo "Hello, who am I talking to?"
 
-echo Hello, who am I talking to?
+# Reading in a value from the user.
+read VARNAME
 
-read varname
+# Display the variable input by the user, back to the user in a message.
+echo "It's nice to meet you $VARNAME"
 
-echo It\'s nice to meet you $varname 
+
