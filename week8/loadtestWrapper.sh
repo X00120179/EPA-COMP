afaf2d95d4ff1b9586992295ad44eab1676b9d4a
epa <epa@localhost.localdomain>
2017-11-17 10:59:15 +0000

    Week 8 bash script updated with for loop

--- week8/loadtestWrapper.sh
+++ week8/loadtestWrapper.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # 1. Invoke load test
-echo " "
-echo "******** Invoking the loadtest ********"
-./loadtest $1 &
+for i in {0 .. $2}
+do
+	echo " "
+	echo "******** Invoking the loadtest ********"
+	./loadtest $i &
 
-# 2. Sleep for the duration of the loadtest T
-echo " "
-echo "******** Sleeping for the duration of the loadtest ********"
-sleep $2
+	# 2. Sleep for the duration of the loadtest T
+	echo " "
+	echo "******** Sleeping for the duration of the loadtest ********"
+	sleep $2
 
-# 3. Pkill the loadtest
-echo " "
-echo "******** Killing the loadtest ********"
-pkill loadtest
+	# 3. Pkill the loadtest
+	echo " "
+	echo "******** Killing the loadtest ********"
+	pkill loadtest
+done
