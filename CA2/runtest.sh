d17640c1ee8954f1b976f49d4f3a0c2e8b886cbb
epa <epa@localhost.localdomain>
2017-12-11 13:13:09 +0000

    runtest script complete/w idle util.

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
+# Format for neat display
+echo -e "C0 \t N \t idle" > results.dat
 
-# 
+# Loop
+for i in {1..50}
+do
+	# Run loadtest.C
+	echo "Running the loadtest $i..."
+	./loadtest $i&
+
+	# Sleep loadtest for 5
+	echo "Loadtest $i is sleeping..."
+	sleep 5
+
+	# Number of completions in synthetic.dat
+	C=`cat synthetic.dat | wc -l`
+
+	# Collect CPU utilization
+        idle=`mpstat 5 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
+	echo "$idle"
+
+	# Kill the loadtest
+	echo "Loadtest $i has been terminated..."
+	pkill loadtest
+
+	echo -e "$C \t $i \t $idle" >> results.dat
+
+	# Remove synthetic.dat file every loop as it regenerates.
+	rm synthetic.dat
+done 
